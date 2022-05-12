6df38299847e6cc778bc8a2d62462a9dd27bdfe8
sieun <sieun0115@ajou.ac.kr>
2022-05-12 19:19:39 +0900

    가상 키보드 - 마우스 이벤트 적용

--- virtual_keyboard/src/js/keyboard.js
+++ virtual_keyboard/src/js/keyboard.js
@@ -5,6 +5,8 @@ export class Keyboard {
   #keyboardEl;
   #inputGroupEl;
   #inputEl;
+  #keyPress = false;
+  #mouseDown = false;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -25,6 +27,11 @@ export class Keyboard {
     document.addEventListener("keydown", this.#onKeyDown.bind(this));
     document.addEventListener("keyup", this.#onKeyUp.bind(this));
     this.#inputEl.addEventListener("input", this.#onInput.bind(this));
+    this.#keyboardEl.addEventListener(
+      "mousedown",
+      this.#onMouseDown.bind(this)
+    );
+    document.addEventListener("mouseup", this.#onMouseUp.bind(this));
   }
 
   #onChangeTheme(event) {
@@ -40,14 +47,40 @@ export class Keyboard {
     this.#inputEl.value = event.target.value.replace(/[ㄱ-ㅎㅏ-ㅣ가-힣]/, "");
   }
   #onKeyDown(event) {
+    if (this.#mouseDown) return;
+    this.#keyPress = true;
     this.#inputGroupEl.classList.toggle("error", event.key === "Process");
     this.#keyboardEl
       .querySelector(`[data-code=${event.code}]`)
       ?.classList.add("active");
   }
   #onKeyUp(event) {
+    if (this.#mouseDown) return;
+    this.#keyPress = false;
     this.#keyboardEl
       .querySelector(`[data-code=${event.code}]`)
       ?.classList.remove("active");
+  }
+  #onMouseDown(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = true;
+    event.target.closest("div.key")?.classList.add("active");
+  }
+  #onMouseUp(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = false;
+    const keyEl = event.target.closest("div.key");
+    const isActive = !!keyEl?.classList.contains("active");
+    const keyValue = keyEl?.dataset.val;
+    if (isActive && keyValue) {
+      if (keyValue === "Backspace") {
+        this.#inputEl.value = this.#inputEl.value.slice(0, -1);
+      } else if (keyValue === "Space") {
+        this.#inputEl.value += " ";
+      } else {
+        this.#inputEl.value += keyValue;
+      }
+    }
+    this.#keyboardEl.querySelector(".active")?.classList.remove("active");
   }
 }
