e413edb14d138e22ae19ba9080339546a60fa1b8
sieun <sieun0115@ajou.ac.kr>
2022-05-27 00:58:36 +0900

    slider - auto play 버튼 추가

--- slider/src/js/imageSlider.js
+++ slider/src/js/imageSlider.js
@@ -2,11 +2,14 @@ class ImageSlider {
   #currentPosition = 0;
   #slideCnt = 0;
   #slideWidth = 0;
+  #intervalId;
+  #autoPlay = true;
   sliderWrapEl;
   sliderListEl;
   nextBtnEl;
   prevBtnEl;
   indicatorEl;
+  autoBtn;
 
   constructor() {
     this.assignElement();
@@ -15,6 +18,7 @@ class ImageSlider {
     this.initSliderListWidth();
     this.createIndicator();
     this.setIndicator();
+    this.initAutoplay();
     this.addEvent();
   }
   assignElement() {
@@ -23,24 +27,34 @@ class ImageSlider {
     this.nextBtnEl = this.sliderWrapEl.querySelector(".next");
     this.prevBtnEl = this.sliderWrapEl.querySelector(".prev");
     this.indicatorEl = this.sliderWrapEl.querySelector(".indicator");
+    this.autoBtn = this.sliderWrapEl.querySelector(".autoBtn");
   }
+
   initSlideCnt() {
     this.#slideCnt = this.sliderListEl.querySelectorAll("li").length;
   }
+
   initSlideWidth() {
     this.#slideWidth = this.sliderWrapEl.clientWidth;
     console.log("width", this.#slideWidth, this.sliderWrapEl.clientWidth);
   }
+
   initSliderListWidth() {
     console.log(this.#slideWidth, this.#slideCnt);
     console.log(this.#slideWidth * this.#slideCnt);
     this.sliderListEl.style.width = `${this.#slideWidth * this.#slideCnt}px`;
   }
+
   initIndicator() {
     this.indicatorLiEl.forEach((li, idx) => {
       if (idx === this.#currentPosition) li.classList.add("active");
     });
   }
+
+  initAutoplay() {
+    this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+  }
+
   addEvent() {
     this.nextBtnEl.addEventListener("click", this.moveToRight.bind(this));
     this.prevBtnEl.addEventListener("click", this.moveToLeft.bind(this));
@@ -48,7 +62,9 @@ class ImageSlider {
       "click",
       this.onClickIndicotor.bind(this)
     );
+    this.autoBtn.addEventListener("click", this.onClickAutoBtn.bind(this));
   }
+
   moveToRight() {
     if (this.#currentPosition === this.#slideCnt - 1) {
       this.#currentPosition = 0;
@@ -57,8 +73,13 @@ class ImageSlider {
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
     this.setIndicator();
   }
+
   moveToLeft() {
     if (this.#currentPosition === 0) {
       this.#currentPosition = this.#slideCnt - 1;
@@ -66,8 +87,13 @@ class ImageSlider {
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
     this.setIndicator();
   }
+
   createIndicator() {
     const docFragment = document.createDocumentFragment();
     for (let i = 0; i < this.#slideCnt; i++) {
@@ -77,12 +103,14 @@ class ImageSlider {
     }
     this.indicatorEl.appendChild(docFragment);
   }
+
   setIndicator() {
     this.indicatorEl.querySelector("li.active")?.classList.remove("active");
     this.indicatorEl
       .querySelector(`li:nth-child(${this.#currentPosition + 1})`)
       .classList.add("active");
   }
+
   onClickIndicotor(e) {
     if (e.target.dataset.index) {
       this.#currentPosition = +e.target.dataset.index;
@@ -91,6 +119,13 @@ class ImageSlider {
       }px`;
       this.setIndicator();
     }
+  }
+
+  onClickAutoBtn() {
+    this.autoBtn.classList.toggle("play");
+    this.#autoPlay = !this.#autoPlay;
+    if (this.#autoPlay) this.initAutoplay();
+    else clearInterval(this.#intervalId);
   }
 }
 
