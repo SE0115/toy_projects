9073a14188274c40b155479a9afb80a0167952ed
sieun <sieun0115@ajou.ac.kr>
2022-06-03 00:44:09 +0900

    drag&drop으로 파일 추가 기능 추가

--- image_gallery/src/App.tsx
+++ image_gallery/src/App.tsx
@@ -1,15 +1,102 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  ChangeEvent,
+} from "react";
 import ImageBox from "./components/ImageBox";
 import "./App.css";
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const dragRef = useRef<HTMLDivElement>(null);
 
   const [imageList, setImageList] = useState<string[]>([]);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  const dragInHandler = useCallback((event: DragEvent): void => {
+    event.preventDefault();
+    event.stopPropagation();
+  }, []);
+
+  const dragOutHandler = useCallback((event: DragEvent): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setIsDragging(false);
+  }, []);
+
+  const dragOverHandler = useCallback((event: DragEvent): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (event.dataTransfer!.files) {
+      setIsDragging(true);
+    }
+  }, []);
+
+  const dropHandler = useCallback((event: DragEvent): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    onChangeFiles(event);
+    setIsDragging(false);
+  }, []);
+
+  const initDragEvents = useCallback((): void => {
+    if (dragRef.current !== null) {
+      dragRef.current.addEventListener("dragenter", dragInHandler);
+      dragRef.current.addEventListener("dragleave", dragOutHandler);
+      dragRef.current.addEventListener("dragover", dragOverHandler);
+      dragRef.current.addEventListener("drop", dropHandler);
+    }
+  }, [dragInHandler, dragOutHandler, dragOverHandler, dropHandler]);
+
+  const resetDragEvents = useCallback((): void => {
+    if (dragRef.current !== null) {
+      dragRef.current.removeEventListener("dragenter", dragInHandler);
+      dragRef.current.removeEventListener("dragleave", dragOutHandler);
+      dragRef.current.removeEventListener("dragover", dragOverHandler);
+      dragRef.current.removeEventListener("drop", dropHandler);
+    }
+  }, [dragInHandler, dragOutHandler, dragOverHandler, dropHandler]);
+
+  const onChangeFiles = useCallback(
+    (event: ChangeEvent<HTMLInputElement> | any): void => {
+      let files: Blob[] = [];
+      if (event.type === "drop") {
+        files = [...event.dataTransfer.files];
+      } else {
+        files = [...event.target.files];
+      }
+
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file) {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onloadend = (event) => {
+            setImageList((prev) => [...prev, event.target?.result as string]);
+          };
+        }
+      }
+    },
+    [imageList]
+  );
+
+  useEffect(() => {
+    initDragEvents();
+
+    return () => resetDragEvents();
+  }, [initDragEvents, resetDragEvents]);
 
   return (
-    <div className="container">
-      <div className={"gallrey-box" + (imageList.length > 0 ? " row" : "")}>
+    <div className={"container" + (isDragging ? " dragging" : "")}>
+      <div
+        className={"gallrey-box" + (imageList.length > 0 ? " row" : "")}
+        ref={dragRef}
+      >
         {imageList.map((img, idx) => (
           <ImageBox key={idx} src={img} />
         ))}
@@ -19,29 +106,7 @@ function App() {
             이미지를 추가해주세요.
           </div>
         )}
-        <input
-          type="file"
-          multiple
-          ref={inputRef}
-          onChange={(event) => {
-            const fileCnt = event.target.files?.length;
-            if (fileCnt) {
-              for (let i = 0; i < fileCnt; i++) {
-                const file = event.target.files?.[i];
-                if (file) {
-                  const reader = new FileReader();
-                  reader.readAsDataURL(file);
-                  reader.onloadend = (event) => {
-                    setImageList((prev) => [
-                      ...prev,
-                      event.target?.result as string,
-                    ]);
-                  };
-                }
-              }
-            }
-          }}
-        />
+        <input type="file" multiple ref={inputRef} onChange={onChangeFiles} />
         <div
           className="addImageBtn"
           onClick={() => {
